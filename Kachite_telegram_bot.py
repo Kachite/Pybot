68103ac129893e897b7f8b9fd86d060a1e859edc
Uzomba Onyekachi Victor <victor.uzomba@yahoo.com>
2025-03-21 22:35:28 +0300

    first commit

---
+++ Kachite_telegram_bot.py
@@ -0,0 +1,17 @@
+import telebot
+
+# Replace 'YOUR_BOT_API_TOKEN' with the token you got from BotFather
+bot = telebot.TeleBot("7556909227:AAGBffoAVMnnwRm34VLAN68IWqhhpFO1TTQ")
+
+# Respond to the /start or /help commands
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello! How can I assist you today?")
+
+# Echo any other messages
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+# Start the bot
+bot.polling()
